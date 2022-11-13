7493ef5c1d7f0c5ff52d3efc09d2283cd6835fcf
IlyaKonovaloB <103986019+IlyaKonovaloB@users.noreply.github.com>
2022-11-13 23:27:26 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+import { faker } from '@faker-js/faker/locale/ru';
+
+import express from 'express';
+const app = express();
+
+const host = '127.0.0.1'
+const port = 3000
+
+function generator() {
+  let users = []
+  for (let i=0; i < 100; i++) {
+    let Name = faker.name.Name();
+    let SurName = faker.name.SurName();
+    let index = faker.address.index();
+    let car = faker.vehicle.vehicle();
+    let telephone = faker.phone.number('+7-###-###-##-##');
+
+    users.push({
+        "userid": i,
+        "name": Name,
+        "sur_name": SurName,
+        "adress": index,
+        "car": car,
+        "telephone": telephone
+    });
+  }
+
+  return { "data": users }
+}
+
+app.get('/user', (req, res) => {
+  res.status(200).type('text/plain')
+  res.send(JSON.stringify(generator(), null, '\t'))
+})
+
+app.listen(port, host, function () {
+  console.log(`Server listens http://${host}:${port}`)
+})
